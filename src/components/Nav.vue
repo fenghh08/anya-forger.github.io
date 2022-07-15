de535ec634a67c8f9e39e492787b5d1c3598c02d
fenghh08 <108651421+fenghh08@users.noreply.github.com>
2022-07-15 17:49:21 +0800

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -55,7 +55,7 @@ const links = [
             >
                 <img
                     :src="pic.uniform"
-                    class="w-full scale-[180%] transition-all duration-200 hover: hover:pt-5 md:hover:pt-7"
+                    class="w-full scale-[180%] transition-all duration-200 hover:rounded-xl hover:pt-5 md:hover:pt-7"
                 />
             </div>
         </transition>
